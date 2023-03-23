92d7aee80f564643c964f88703bf6d6845974f4e
jve <joe.veyhl@gmail.com>
2023-03-23 19:30:45 -0400

    added User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,29 @@
 //to-do setup user data model based on figma
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

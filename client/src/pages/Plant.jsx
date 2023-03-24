449d320ec3e92f709e304bc3cb8b99d60a96a71b
Xuan Zou <zoumacy@gmail.com>
2023-03-24 17:34:05 -0400

    added pages & layouts & hooks

---
+++ client/src/pages/Plant.jsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import Input, {  Textarea } from '../components/utils/Input';
+import Loader from '../components/utils/Loader';
+import useFetch from '../hooks/useFetch';
+import MainLayout from '../layouts/MainLayout';
+import validateManyFields from '../validations';
+
+const Plant = () => {
+
+  const authState = useSelector(state => state.authReducer);
+  const navigate = useNavigate();
+  const [fetchData, { loading }] = useFetch();
+  const { plantId } = useParams();
+
+  const mode = plantId === undefined ? "add" : "update";
+  const [plant, setPlant] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: ""
+  });
+  const [formErrors, setFormErrors] = useState({});
+
+
+  useEffect(() => {
+    document.title = mode === "add" ? "Add plant" : "Update Plant";
+  }, [mode]);
+
+
+  useEffect(() => {
+    if (mode === "update") {
+      const config = { url: `/plants/${plantId}`, method: "get", headers: { Authorization: authState.token } };
+      fetchData(config, { showSuccessToast: false }).then((data) => {
+        setPlant(data.plant);
+        setFormData({ name: data.plant.name, description: data.plant.description });
+      });
+    }
+  }, [mode, authState, plantId, fetchData]);
+
+
+
+  const handleChange = e => {
+    setFormData({
+      ...formData, [e.target.name]: e.target.value
+    });
+  }
+
+  const handleReset = e => {
+    e.preventDefault();
+    setFormData({
+      name: plant.name,
+      description: plant.description
+    });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const errors = validateManyFields("plant", formData);
+    setFormErrors({});
+
+    if (errors.length > 0) {
+      setFormErrors(errors.reduce((total, ob) => ({ ...total, [ob.field]: ob.err }), {}));
+      return;
+    }
+
+    if (mode === "add") {
+      const config = { url: "/plants", method: "post", data: formData, headers: { Authorization: authState.token } };
+      fetchData(config).then(() => {
+        navigate("/");
+      });
+    }
+    else {
+      const config = { url: `/plants/${plantId}`, method: "put", data: formData, headers: { Authorization: authState.token } };
+      fetchData(config).then(() => {
+        navigate("/");
+      });
+    }
+  }
+
+
+  const fieldError = (field) => (
+    <p className={`mt-1 text-pink-600 text-sm ${formErrors[field] ? "block" : "hidden"}`}>
+      <i className='mr-2 fa-solid fa-circle-exclamation'></i>
+      {formErrors[field]}
+    </p>
+  )
+
+  return (
+    <>
+      <MainLayout>
+        <form className='m-auto my-16 max-w-[1000px] bg-white p-8 border-2 shadow-md rounded-md'>
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <h2 className='text-center mb-4'>{mode === "add" ? "Add New Plant" : "Edit Plant"}</h2>
+              <div className="mb-4">
+                <label htmlFor="name">Name</label>
+                <Input type="name" name="name" id="name" value={formData.name} placeholder="Name of Plant" onChange={handleChange} />
+                {fieldError("name")}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="description">Description</label>
+                <Textarea type="description" name="description" id="description" value={formData.description} placeholder="Write here.." onChange={handleChange} />
+                {fieldError("description")}
+              </div>
+
+              <button className='bg-primary text-white px-4 py-2 font-medium hover:bg-primary-dark' onClick={handleSubmit}>{mode === "add" ? "Add plant" : "Update Plant"}</button>
+              <button className='ml-4 bg-red-500 text-white px-4 py-2 font-medium' onClick={() => navigate("/")}>Cancel</button>
+              {mode === "update" && <button className='ml-4 bg-blue-500 text-white px-4 py-2 font-medium hover:bg-blue-600' onClick={handleReset}>Reset</button>}
+            </>
+          )}
+        </form>
+      </MainLayout>
+    </>
+  )
+}
+
+export default Plant

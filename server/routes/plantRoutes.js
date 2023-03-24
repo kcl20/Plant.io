c3147db21383e216750b51aef4d9724e2b821420
xinnnan <xinnancao@gmail.com>
2023-03-23 20:33:33 -0400

    updated

--- server/routes/plantRoutes.js
+++ server/routes/plantRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = require('router');
 const { getAllPlants, getPlant, addPlant, updatePlant, deletePlant } = require("../controllers/plantControllers");
-
+const { verifyAccessToken } = require("../middlewares.js/index.js");
 
 // api routes
-router.get("/", getAllPlants);
-router.get("/:plantId", getPlant);
-router.post("/", addPlant);
-router.put("/:plantId", updatePlant);
-router.delete("/:plantId", deletePlant);
+router.get("/", verifyAccessToken,getAllPlants);
+router.get("/:plantId", verifyAccessToken,getPlant);
+router.post("/", verifyAccessToken,addPlant);
+router.put("/:plantId", verifyAccessToken,updatePlant);
+router.delete("/:plantId", verifyAccessToken,deletePlant);
 
 module.exports = router;

22d95fcb620089533dac60c29ed9cf63eb5151ec
kcl20 <kcl20@outlook.com>
2023-03-25 02:40:56 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Plant from "./pages/Plant";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { saveProfile } from "./redux/actions/authActions";
 
 function App() {
+
+  const authState = useSelector(state => state.authReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    dispatch(saveProfile(token));
+  }, [authState.isLoggedIn, dispatch]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={authState.isLoggedIn ? <Navigate to="/" /> : <Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/plants/add" element={authState.isLoggedIn ? <Plant /> : <Navigate to="/login" state={{ redirectUrl: "/plants/add" }} />} />
+          <Route path="/plants/:plantId" element={authState.isLoggedIn ? <Plant /> : <Navigate to="/login" state={{ redirectUrl: window.location.pathname }} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

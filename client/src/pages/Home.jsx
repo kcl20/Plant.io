17f08412a79a6e17803a01b4f4f96b888c8cd561
jve <joe.veyhl@gmail.com>
2023-03-30 18:10:14 -0400

    Added DonationButton component for Stripe

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,42 +1,54 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Allplants from '../Allplants';
-import Plants from '../components/Plants';
-import MainLayout from '../layouts/MainLayout';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Allplants from "../Allplants";
+import Plants from "../components/Plants";
+import MainLayout from "../layouts/MainLayout";
+import DonationButton from "../components/DonationButton";
 
 const Home = () => {
-
-  const authState = useSelector(state => state.authReducer);
+  const authState = useSelector((state) => state.authReducer);
   const { isLoggedIn } = authState;
 
   useEffect(() => {
-    document.title = authState.isLoggedIn ? `${authState.user.name}'s plants` : "Plant.io";
+    document.title = authState.isLoggedIn
+      ? `${authState.user.name}'s plants`
+      : "Plant.io";
   }, [authState]);
-
-
 
   return (
     <>
       <MainLayout>
         {!isLoggedIn ? (
-          <div className='bg-primary text-white py-8 text-center'>
-            <h1 className='text-2xl'> Welcome to Plant.io</h1>
-            <Link to="/signup" className='mt-10 text-xl block space-x-2 hover:space-x-4'>
-              <span className='transition-[margin]'>Your ultimate app for plant enthusiasts and green thumbs!</span>
-              <span className='relative ml-4 text-base transition-[margin]'><i className="fa-solid fa-arrow-right"></i></span>
+          <div className="bg-primary text-white py-8 text-center">
+            <h1 className="text-2xl"> Welcome to Plant.io</h1>
+            <Link
+              to="/signup"
+              className="mt-10 text-xl block space-x-2 hover:space-x-4"
+            >
+              <span className="transition-[margin]">
+                Your ultimate app for plant enthusiasts and green thumbs!
+              </span>
+              <span className="relative ml-4 text-base transition-[margin]">
+                <i className="fa-solid fa-arrow-right"></i>
+              </span>
             </Link>
-            <><Allplants /></>
+            <>
+              <Allplants />
+            </>
           </div>
         ) : (
           <>
-            <h1 className='text-lg mt-8 mx-8 border-b border-b-gray-300'>Welcome {authState.user.name}</h1>
+            <h1 className="text-lg mt-8 mx-8 border-b border-b-gray-300">
+              Welcome {authState.user.name}
+            </h1>
             <Plants />
           </>
         )}
+        <DonationButton />
       </MainLayout>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
